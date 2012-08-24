7ad9190182caf9089a7ed5e657066bbe8260f89e
Kris Jenkins <krisajenkins@gmail.com>
2012-08-24 12:08:52 +0100

    Adding a few more moves tests.

--- test/tictactoe_clojure/test/core.clj
+++ test/tictactoe_clojure/test/core.clj
@@ -15,3 +15,20 @@
   (is (= (move empty-board 2 1) [[:_ :_ :_]
                                  [:_ :_ :_]
                                  [:_ :X :_]])))
+
+(deftest test-subsequent-moves
+  (is (= (-> empty-board
+             (move 0 0)
+             (move 1 1)
+             (move 0 2))
+         [[:X :_ :X]
+          [:_ :O :_]
+          [:_ :_ :_]]))
+
+  (is (= (move [[:X :X :_]
+                [:O :O :_]
+                [:_ :_ :_]]
+               0 2)
+         [[:X :X :X]
+          [:O :O :_]
+          [:_ :_ :_]])))
