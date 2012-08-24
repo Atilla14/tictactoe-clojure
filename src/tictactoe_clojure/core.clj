5b2f58711d5a5feeb7c1816b7b01fc296766c0c6
Kris Jenkins <krisajenkins@gmail.com>
2012-08-24 13:32:53 +0100

    Refactoring heavily.

--- src/tictactoe_clojure/core.clj
+++ src/tictactoe_clojure/core.clj
@@ -1,38 +1,49 @@
 (ns tictactoe-clojure.core)
 
-; Private
-;
-(defn- count-in [coll item]
-  (count (filter #(= item %) (flatten coll))))
-
-(defn- next-mover [board]
-  (if (> (count-in board :X)
-         (count-in board :O))
-    :O
-    :X))
-
-(defn- any-equal [value coll]
-  (some #(= value %) coll))
-
-(defn- horizontal-win [board player]
-  (any-equal 3 [(count-in (get board 0) player)
-                (count-in (get board 1) player)
-                (count-in (get board 2) player)]))
+; Private, generic.
 
 (defn- third [coll] (get coll 2))
 
+(defn- equal-to [x]
+  (fn [y] (= x y)))
+
+(defn- count-in
+  "Counts the occurances of item in a collection. Includes any nested occurances."
+  [item coll]
+  (->> coll
+       flatten
+       (filter (equal-to item))
+       count))
+
+(defn- count-is [value item coll]
+  (= value
+     (count-in item coll)))
+
+; Private, specific.
+
+(defn- next-mover [board]
+  (if (> (count-in :X board)
+         (count-in :O board))
+    :O
+    :X))
+
+(defn- horizontal-win [board player]
+  (or (count-is 3 player (first  board))
+      (count-is 3 player (second board))
+      (count-is 3 player (third  board))))
+
 (defn- vertical-win [board player]
-  (any-equal 3 [(count-in (map first  board) player)
-                (count-in (map second board) player)
-                (count-in (map third  board) player)]))
+  (or (count-is 3 player (map first  board))
+      (count-is 3 player (map second board))
+      (count-is 3 player (map third  board))))
 
 (defn- diagonal-win [board player]
-  (any-equal 3 [(count-in [(get-in board [0 0])
-                           (get-in board [1 1])
-                           (get-in board [2 2])] player)
-                (count-in [(get-in board [0 2])
-                           (get-in board [1 1])
-                           (get-in board [2 0])] player)]))
+  (or (count-is 3 player [(get-in board [0 0])
+                          (get-in board [1 1])
+                          (get-in board [2 2])])
+      (count-is 3 player [(get-in board [0 2])
+                          (get-in board [1 1])
+                          (get-in board [2 0])])))
 
 (defn- win-for [player board]
   (or (horizontal-win board player)
@@ -49,11 +60,11 @@
   (update-in board [row column] (fn [_] (next-mover board))))
 
 (defn status [board]
-  (cond (= 9 (count-in board :_)) :empty
+  (cond (count-is 9 :_ board) :empty
 
         (or (win-for :X board)
             (win-for :O board)) :win
 
-        (= 0 (count-in board :_)) :draw
+        (count-is 0 :_ board) :draw
 
         :else :ongoing))
